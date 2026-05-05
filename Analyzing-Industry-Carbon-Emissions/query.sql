4c1f6e11b49dd39cb42190b78d41d6e20714bef9
georlia <149323475+georlia@users.noreply.github.com>
2026-05-05 16:10:42 +0300

    Delete Analyzing-Industry-Carbon-Emissions directory

--- Analyzing-Industry-Carbon-Emissions/query.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	industry_group,
-	COUNT(DISTINCT company) AS num_companies,
-	ROUND(SUM(carbon_footprint_pcf),1) AS total_industry_footprint
-FROM product_emissions
-WHERE year = 2017
-GROUP BY industry_group,year
-ORDER BY total_industry_footprint DESC;
