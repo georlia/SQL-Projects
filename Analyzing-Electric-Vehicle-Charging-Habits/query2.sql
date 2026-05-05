870b04963966f348095f7211f261a39bdcef9d29
georlia <149323475+georlia@users.noreply.github.com>
2026-05-05 17:13:41 +0300

    Delete Analyzing-Electric-Vehicle-Charging-Habits directory

--- Analyzing-Electric-Vehicle-Charging-Habits/query2.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	weekdays_plugin,
-	start_plugin_hour,
-	COUNT(start_plugin) AS num_charging_sessions
-FROM charging_sessions
-WHERE user_type = 'Shared'
-GROUP BY weekdays_plugin, start_plugin_hour
-ORDER BY num_charging_sessions DESC
-LIMIT 10;
--- Analyzing-Electric-Vehicle-Charging-Habits/query3.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	user_id, 
-	AVG(duration_hours) AS avg_charging_duration
-FROM charging_sessions
-WHERE user_type = 'Shared'
-GROUP BY user_id
-HAVING AVG(duration_hours) > 10
-ORDER BY avg_charging_duration DESC;
